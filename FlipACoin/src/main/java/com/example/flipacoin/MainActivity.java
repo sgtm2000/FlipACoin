cba0221d2616e3eaf324fb97b64717564d87d220
Sambridi Gautam <sambridi.gautam@gmail.com>
2013-07-22 22:40:48 -0700

    Initial Commit

---
+++ FlipACoin/src/main/java/com/example/flipacoin/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.flipacoin;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void flipCoin(View view){
+        ImageView head = (ImageView) findViewById(R.id.headPic);
+        ImageView tail = (ImageView) findViewById(R.id.tailPic);
+        double coinStatus = Math.random();
+
+        if (coinStatus <= 0.5){
+            tail.setVisibility(View.INVISIBLE);
+            head.setVisibility(View.VISIBLE);
+        }
+        else{
+            head.setVisibility(View.INVISIBLE);
+            tail.setVisibility(View.VISIBLE);
+        }
+
+        Button clearButton = (Button) findViewById(R.id.clearButton);
+        clearButton.setVisibility(View.VISIBLE);
+    }
+
+    public void clear(View view){
+        ImageView head = (ImageView) findViewById(R.id.headPic);
+        ImageView tail = (ImageView) findViewById(R.id.tailPic);
+        head.setVisibility(View.INVISIBLE);
+        tail.setVisibility(View.INVISIBLE);
+
+        Button clearButton = (Button) findViewById(R.id.clearButton);
+        clearButton.setVisibility(View.INVISIBLE);
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+Version 0.1
+- Simple Android app for flipping a coin
+- Added basic functionality, head and tail images, and a clear button
+
+Future enhancements
+- Save the state of the coin when the screen orientation is changed
+- Add a fancy gif that shows a spinning coin when the "Head or Tail" button is pushed
+- Add a nicer icon
+
+This is a gradle project built using Android studio. 
